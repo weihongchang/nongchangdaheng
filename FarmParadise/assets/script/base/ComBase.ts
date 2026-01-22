1b317a36e71a9571910be34ab6c0d7fc3b1607a2
weihongchang <328261560@qq.com>
2026-01-22 18:18:55 +0800

    优化

--- FarmParadise/assets/script/base/ComBase.ts
+++ FarmParadise/assets/script/base/ComBase.ts
@@ -1,30 +1,32 @@
-import { _decorator, Component, Node, __private } from 'cc';
+import { _decorator, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
+
+type Constructor<T> = new (...args: any[]) => T;
 
 @ccclass('ComBase')
 export class ComBase extends Component {
     comMap = new Map<string, Node>();
-    addCom<T extends Component>(classConstructor: __private._types_globals__Constructor<T>): T | null {
+    
+    addCom<T extends Component>(classConstructor: Constructor<T>): T | null {
         var obj = new Node();
         obj.name = classConstructor.prototype.name;
         obj.parent = this.node;
         var com = obj.addComponent(classConstructor);
         this.comMap.set(classConstructor.prototype.name, obj);
-        return com;
+        return com as T;
     }
 
-    getCom<T extends Component>(classConstructor: __private._types_globals__Constructor<T> | __private._types_globals__AbstractedConstructor<T>): T | null {
+    getCom<T extends Component>(classConstructor: Constructor<T>): T | null {
         var obj = this.comMap.get(classConstructor.prototype.name);
         if (obj != null)
             return obj.getComponent(classConstructor);
         return null;
     }
 
-    removeCom<T extends Component>(classConstructor: __private._types_globals__Constructor<T> | __private._types_globals__AbstractedConstructor<T>): void {
+    removeCom<T extends Component>(classConstructor: Constructor<T>): void {
         var obj = this.comMap.get(classConstructor.prototype.name);
         if (obj != null)
             obj.destroy();
         this.comMap.delete(classConstructor.prototype.name);
     }
 }
-
